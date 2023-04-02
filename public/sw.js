fd38a1e9a2ae9b9b16008e4b86ea8d8e30ea4e31
BrightonDutson <102453602+BrightonDutson@users.noreply.github.com>
2023-04-02 11:22:23 -0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,13 +3,32 @@ if ('function' === typeof importScripts) {
     importScripts('/osana/osana.worker.js');
     const sw = new DIPServiceWorker('/dip/dip.worker.js');
     const Osana = new OsanaServiceWorker();
+
+    // Register Tor service worker
+    const torWorker = new ServiceWorker('/tor/tor-sw.js', { scope: '/tor/' });
+    torWorker.register();
+
     self.addEventListener('fetch', (event) => {
         if (event.request.url.startsWith(location.origin + '/service/dip/'))
             event.respondWith(sw.fetch(event));
         if (event.request.url.startsWith(location.origin + '/service/~osana/'))
             event.respondWith(Osana.fetch(event));
+
+        // Handle Tor proxy requests
+        if (event.request.url.startsWith(location.origin + '/tor/go/')) {
+            const url = decodeURIComponent(event.request.url.replace(location.origin + '/tor/go/', ''));
+            event.respondWith(
+                fetch(url, { 
+                    mode: 'cors',
+                    headers: {
+                        'X-Tor-Proxy': 'true',
+                    }
+                })
+            );
+        }
     });
 }
+
 assets = [
     '/',
     '/404',
